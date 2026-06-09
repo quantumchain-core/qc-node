62be22f12aa7826676214d8eb1d44d5ce2bf4d54
quantumchain-core <touqeerahmadofficial896@gmail.com>
2026-06-09 10:01:25 +0500

    fix: update net module for libp2p 0.53 API

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,12 +1,15 @@
 use libp2p::{
     gossipsub, mdns, noise,
     swarm::{NetworkBehaviour, SwarmEvent},
-    tcp, yamux, SwarmBuilder,
+    tcp, yamux, 
+    Swarm, SwarmBuilder,
 };
+use libp2p::futures::StreamExt;
 use std::error::Error;
 use tokio::time::{Duration, interval};
-use crate::config::CHAIN_ID;
+use crate::crypto::dilithium::RAMPURA_TESTNET_0_CHAIN_ID;
 
+// This macro generates QtcBehaviourEvent automatically
 #[derive(NetworkBehaviour)]
 pub struct QtcBehaviour {
     pub gossipsub: gossipsub::Behaviour,
@@ -41,41 +44,37 @@ pub async fn start_network() -> Result<(), Box<dyn Error>> {
         })?
         .build();
 
-    // Topic for qtc-rampura-testnet-0 blocks
-    let topic = gossipsub::IdentTopic::new(CHAIN_ID);
+    let topic = gossipsub::IdentTopic::new(RAMPURA_TESTNET_0_CHAIN_ID);
     swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
 
-    // Listen on all interfaces, random port for mobile
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    println!("Started P2P node for {}", CHAIN_ID);
+    println!("Started P2P node for {}", RAMPURA_TESTNET_0_CHAIN_ID);
     println!("Local PeerId: {}", swarm.local_peer_id());
 
-    // Keepalive + block announce loop
-    let mut tick = interval(Duration::from_secs(3)); // 3s block time
+    let mut tick = interval(Duration::from_secs(3));
     loop {
         tokio::select! {
             _ = tick.tick() => {
-                // M2: Announce fake block for testing
-                let block_data = format!("block-{}-{}", CHAIN_ID, chrono::Utc::now().timestamp());
+                let block_data = format!("block-{}-{}", RAMPURA_TESTNET_0_CHAIN_ID, chrono::Utc::now().timestamp());
                 if let Err(e) = swarm.behaviour_mut().gossipsub.publish(topic.clone(), block_data.as_bytes()) {
                     println!("Publish error: {:?}", e);
                 }
             }
-            event = swarm.select_next_some() => {
+            event = swarm.next().await => {
                 match event {
-                    SwarmEvent::NewListenAddr { address, .. } => {
+                    Some(SwarmEvent::NewListenAddr { address, .. }) => {
                         println!("Listening on {}", address);
                     }
-                    SwarmEvent::Behaviour(QtcBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+                    Some(SwarmEvent::Behaviour(QtcBehaviourEvent::Mdns(mdns::Event::Discovered(list)))) => {
                         for (peer_id, _multiaddr) in list {
                             println!("mDNS discovered peer: {}", peer_id);
                             swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
                         }
                     }
-                    SwarmEvent::Behaviour(QtcBehaviourEvent::Gossipsub(gossipsub::Event::Message {
+                    Some(SwarmEvent::Behaviour(QtcBehaviourEvent::Gossipsub(gossipsub::Event::Message {
                         message, ..
-                    })) => {
+                    }))) => {
                         println!("Received block: {}", String::from_utf8_lossy(&message.data));
                     }
                     _ => {}
@@ -83,4 +82,4 @@ pub async fn start_network() -> Result<(), Box<dyn Error>> {
             }
         }
     }
-}
+                }
