9128cb0d0cc5fdbe73d5ffe4495d4ecc2500d803
quantumchain-core <touqeerahmadofficial896@gmail.com>
2026-06-10 09:55:17 +0500

    M1: lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,1 @@
-pub mod crypto; // M1
-pub mod net; // M2 
-pub mod chain; // M3
-
-pub use crypto::{
-    DilithiumPubKey,
-    DilithiumSecretKey,
-    DilithiumSig,
-    generate_keypair,
-    sign,
-    verify,
-};
+pub mod crypto;
