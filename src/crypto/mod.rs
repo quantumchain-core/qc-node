e746706c3b5f0a5a4412b6407dc695c092388c67
quantumchain-core <touqeerahmadofficial896@gmail.com>
2026-06-10 09:56:45 +0500

    M1: crypto mod

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -1,13 +1,2 @@
 pub mod dilithium;
-
-// Re-export M1 types so M2/M3 can use them
-pub use dilithium::{
-    DilithiumPubKey, 
-    DilithiumSecretKey, 
-    DilithiumSig,
-    generate_keypair,
-    sign, 
-    verify,
-    hash_sha3,
-    RAMPURA_TESTNET_0_CHAIN_ID,
-};
+pub use dilithium::*;
