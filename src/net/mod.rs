92e25a448083d046dd8d3b55e355b5fae15bc67a
quantumchain-core <touqeerahmadofficial896@gmail.com>
2026-06-10 11:29:21 +0500

    M3: Fix net/mod.rs syntax error

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,8 +1,5 @@
 use libp2p::{gossipsub, swarm::NetworkBehaviour, PeerId};
 use libp2p::identity::Keypair;
-use std::error::Error;       seelibp2p::{gossipsub, noise, swarm::NetworkBehaviour, tcp, yamux, PeerId};
-use libp2p::identity::Keypair;
-use std::error::Error;
 
 pub fn peer_id_from_pk(_pk: &[u8]) -> PeerId {
     // M2: Generate libp2p PeerID. M1 Dilithium key used for blocks only.
@@ -15,12 +12,9 @@ struct QcBehaviour {
     gossipsub: gossipsub::Behaviour,
 }
 
-pub fn new_swarm() -> Result<PeerId, Box<dyn Error>> {
+pub fn new_swarm() -> Result<PeerId, Box<dyn std::error::Error>> {
     let keypair = Keypair::generate_ed25519();
     let peer_id = PeerId::from(keypair.public());
-    
-    // M2: Just prove we can build the behaviour. Full swarm in M3.
-    let _gossipsub_config = gossipsub::Config::default();
     Ok(peer_id)
 }
 
@@ -30,7 +24,7 @@ mod m2_tests {
 
     #[test]
     fn m2_peer_id_works() {
-        let pk = vec![0u8; 1952]; // fake M1 pubkey
+        let pk = vec![0u8; 1952];
         let peer_id = peer_id_from_pk(&pk);
         assert!(!peer_id.to_string().is_empty());
     }
@@ -40,4 +34,4 @@ mod m2_tests {
         let result = new_swarm();
         assert!(result.is_ok());
     }
-    }
+}
