406d7f30d466d7532bdbab4b67fee26a263ad86a
quantumchain-core <touqeerahmadofficial896@gmail.com>
2026-06-10 10:04:52 +0500

    Delete src/config.rs

--- src/config.rs
+++
@@ -1,5 +0,0 @@
-pub const CHAIN_ID: &str = "qtc-rampura-testnet-0";
-pub const BLOCK_TIME_SECS: u64 = 3;
-pub const MAX_BLOCK_SIZE: usize = 500_000; // 500KB
-pub const MAX_MESSAGE_SIZE: usize = 512_000; // 512KB for 3G
-pub const GENESIS_TIMESTAMP: u64 = 1781238000; // 2026-06-12 00:00:00 UTC
