2f01cbe8adafa1f8641e6f86ecfc2721d4a5de92
quantumchain-core <touqeerahmadofficial896@gmail.com>
2026-06-10 12:04:49 +0500

    M4: Fix libp2p 0.44 SwarmBuilder + transport

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,7 +1,11 @@
-use libp2p::{gossipsub, swarm::NetworkBehaviour, PeerId, Swarm, Multiaddr};
+use libp2p::{gossipsub, swarm::NetworkBehaviour, PeerId, Swarm};
 use libp2p::identity::Keypair;
 use libp2p::gossipsub::{MessageAuthenticity, ValidationMode};
-use libp2p::swarm::SwarmEvent;
+use libp2p::swarm::SwarmBuilder;
+use libp2p::core::upgrade;
+use libp2p::noise;
+use libp2p::tcp;
+use libp2p::yamux;
 use std::error::Error;
 
 #[derive(NetworkBehaviour)]
@@ -9,35 +13,32 @@ pub struct QcBehaviour {
     pub gossipsub: gossipsub::Behaviour,
 }
 
-pub async fn start_node() -> Result<(), Box<dyn Error>> {
+pub async fn new_swarm() -> Result<Swarm<QcBehaviour>, Box<dyn Error>> {
     let id_keys = Keypair::generate_ed25519();
     let peer_id = PeerId::from(id_keys.public());
     println!("Local peer id: {peer_id}");
 
     let gossipsub_config = gossipsub::ConfigBuilder::default()
-       .validation_mode(ValidationMode::Strict)
-       .build()
-       .expect("Valid config");
+      .validation_mode(ValidationMode::Strict)
+      .build()
+      .expect("Valid config");
     
-    let mut gossipsub = gossipsub::Behaviour::new(
+    let gossipsub = gossipsub::Behaviour::new(
         MessageAuthenticity::Signed(id_keys.clone()), 
         gossipsub_config
     ).expect("Correct configuration");
 
-    let topic = gossipsub::IdentTopic::new("qc-blocks");
-    gossipsub.subscribe(&topic)?;
-
     let behaviour = QcBehaviour { gossipsub };
-    let mut swarm = Swarm::new(
-        libp2p::core::transport::MemoryTransport::default(),
-        behaviour,
-        peer_id,
-        libp2p::swarm::Config::with_async_std_executor(),
-    );
 
-    swarm.listen_on("/memory/0".parse()?)?;
-    
-    Ok(())
+    let transport = tcp::tokio::Transport::new(tcp::Config::default())
+       .upgrade(upgrade::Version::V1)
+       .authenticate(noise::Config::new(&id_keys)?)
+       .multiplex(yamux::Config::default())
+       .boxed();
+
+    let swarm = SwarmBuilder::with_tokio_executor(transport, behaviour, peer_id).build();
+
+    Ok(swarm)
 }
 
 pub fn peer_id_from_pk(_pk: &[u8]) -> PeerId {
@@ -46,8 +47,16 @@ pub fn peer_id_from_pk(_pk: &[u8]) -> PeerId {
 }
 
 #[cfg(test)]
-mod m2_tests {
+mod m4_tests {
     use super::*;
+
+    #[test]
+    fn m4_swarm_builds() {
+        async_std::task::block_on(async {
+            let swarm = new_swarm().await;
+            assert!(swarm.is_ok());
+        });
+    }
 
     #[test]
     fn m2_peer_id_works() {
