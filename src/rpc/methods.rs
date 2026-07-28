9dcf403bf7a84b3039d0173297e934f6f6044094
quantumchain-core <touqeerahmadofficial896@gmail.com>
2026-07-28 06:42:01 +0500

    Update methods.rs

--- src/rpc/methods.rs
+++ src/rpc/methods.rs
@@ -313,11 +313,10 @@ mod tests {
 
     fn test_state() -> AppState {
         let tmp = tempfile::TempDir::new().unwrap();
-        std::env::set_var("QC_DB_PATH", tmp.path());
         AppState {
             state_db: Arc::new(Mutex::new(StateDB::new())),
             mempool: Arc::new(Mutex::new(Mempool::new(Default::default()))),
-            storage: Arc::new(Storage::new().unwrap()),
+            storage: Arc::new(Storage::open_at(tmp.path()).unwrap()),
             chain_head: Arc::new(Mutex::new(ChainHead::default())),
             outbox: Arc::new(Mutex::new(Vec::new())),
         }
