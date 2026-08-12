60a59b0234d7cc6bd6580a4f4f6fc3e4d9f136dc
quantumchain-core <touqeerahmadofficial896@gmail.com>
2026-08-12 14:03:14 +0500

    Add action field to transaction structure

--- src/node/mod.rs
+++ src/node/mod.rs
@@ -333,6 +333,7 @@ mod tests {
             base_fee: 1_000,
             priority_fee: 50,
             gas_limit: 21_000,
+            action: crate::mempool::TxAction::Transfer,
             signature: Vec::new(),
             received_at: 0,
             from_pubkey: pk,
